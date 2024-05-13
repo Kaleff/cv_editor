9c77cf6e5d3f6f9365039320f5de60a8f3980d95
Marks Kalijevs <markskalijevs@gmail.com>
2024-05-13 19:50:02 +0300

    Add foreign key relationships to models

--- app/Models/Resume.php
+++ app/Models/Resume.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Resume extends Model
 {
     use HasFactory;
+
+    public function experiences(): HasMany
+    {
+        return $this->hasMany(Experience::class);
+    }
 }
