e026512d391222ef3c44a34e81c827f9c44665b6
Marks Kalijevs <markskalijevs@gmail.com>
2024-05-13 13:45:25 +0300

    Change the blade layout for layout and welcome page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,10 +24,13 @@
         @if (Route::has('login'))
             <nav class="-mx-3 flex flex-1 justify-end">
                 @auth
-                    <a href="{{ url('/dashboard') }}"
-                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                        Dashboard
+                    <a href="javascript:void" onclick="document.getElementById('logout-form').submit();">
+                        Logout
                     </a>
+                    
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 @else
                     <a href="{{ route('login') }}"
                         class="">
