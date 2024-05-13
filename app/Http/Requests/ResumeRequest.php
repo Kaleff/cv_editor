3a04ec374dbfd7319be0cf73dbeae0c7459153d3
Marks Kalijevs <markskalijevs@gmail.com>
2024-05-13 13:44:22 +0300

    Create form requests

---
+++ app/Http/Requests/ResumeRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResumeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:5|max:100',
+            'phone' => 'regex:/(+)[0-9]{11}/',
+            'address' => 'required|min:5|max:100'
+        ];
+    }
+}
