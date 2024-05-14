1949a52cbc908eeadf8a8694d86d6b80ec03f2b7
Marks Kalijevs <markskalijevs@gmail.com>
2024-05-15 02:59:54 +0300

    Add name to resume resource

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,26 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome')->name('homepage');
+    if (Auth::check()) {
+        return to_route('resume_list');
+    }
+    return view('welcome');
 });
 
 Route::controller(UserController::class)->group(function () {
     // Login routes
     Route::get('/login', 'loginShow')->name('login');
     Route::post('/login', 'login');
-    Route::post('/logout', 'logout')->name('logout');
     // Registration Routes...
     Route::get('/register', 'registerShow')->name('register');
-    Route::post('/register', 'register');    
+    Route::post('/register', 'register');
+    Route::post('/logout', 'logout')->name('logout');
 });
 
+Route::resource('resume', ResumeController::class)
+    ->name('create', 'resume_create_form')
+    ->name('edit', 'resume_edit_form')
+    ->name('index', 'resume_list')->middleware('auth');
 Route::resources([
-    'resume' => ResumeController::class,
     'experience' => ExperienceController::class
 ]);
