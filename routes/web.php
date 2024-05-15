27bed42749e065bed26527fd19b3f60a85a59141
Marks Kalijevs <markskalijevs@gmail.com>
2024-05-15 12:10:06 +0300

    Add aditional routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
         return to_route('resume_list');
     }
     return view('welcome');
-});
+})->name('homepage');
 
 Route::controller(UserController::class)->group(function () {
     // Login routes
@@ -26,7 +26,13 @@ Route::controller(UserController::class)->group(function () {
 Route::resource('resume', ResumeController::class)
     ->name('create', 'resume_create_form')
     ->name('edit', 'resume_edit_form')
+    ->name('destroy', 'resume_delete')
+    ->name('show', 'resume_show')
     ->name('index', 'resume_list')->middleware('auth');
+
 Route::resource('experience', ExperienceController::class)
-    ->name('create', 'experience_create_form')
-    ->name('edit', 'experience_edit_form');
+    ->name('edit', 'experience_edit_form')
+    ->name('store', 'store_experience')
+    ->name('destroy', 'experience_delete')->middleware('auth');
+
+Route::get('create_experience/{id}', [ExperienceController::class, 'create'])->name('experience_create_form')->middleware('auth');
