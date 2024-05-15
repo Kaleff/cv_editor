9109fc88cfc5cf985b5181e4b0173dd09be0affd
Marks Kalijevs <markskalijevs@gmail.com>
2024-05-15 10:22:04 +0300

    Add fillable to Experience model

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Experience extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'company',
+        'start_date',
+        'end_date',
+        'location',
+        'role',
+        'type',
+        'description',
+        'resume_id'
+    ];
 }
